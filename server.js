8992526b45c3af0fe24ad503f81d6e535431988a
leekt <koreaa8@naver.com>
2019-02-15 13:48:59 +0900

    jQuery, Ajax 통신으로 insert까지

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express()
 var request = require('request');
 var mysql = require('mysql');
 var path = require('path');
+var bodyParser=require("body-parser")
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -12,6 +13,13 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
+app.use(bodyParser.json());//app.use가 미들웨어를 설치해주는 것
+app.use(bodyParser.urlencoded({extended : false}));
+
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, 'views')))
+//app.use(express.json);
+//app.use(express.urlencoded({extended : false}));
 app.set('views', path.join(__dirname, 'views'));//__dirname은 views라는 나의 폴더아래에 있다라고 알려주는 것
 app.set('view engine', 'ejs');
 
@@ -52,4 +60,25 @@ app.get('/join', function(req, res){
 	res.render('join');
 })
 
+app.post('/userJoin', function(req, res){
+	console.log("여기 들어왔습니다");
+	var name = req.body.nameajax;
+	var weight = req.body.weightajax;
+	var age = req.body.ageajax;
+
+	connection.query("INSERT INTO user(id, name, user_id, user_name, user_address) VALUES(?,?,?,'123','koreaa8@va')", [name,weight,age],
+	function(err,result){
+		if(err){
+			console.error(err);
+			throw err;
+		}else{
+			res.json("DATA INPUT");
+		}
+	} 
+	);
+	console.log(name);
+	console.log(weight);
+	console.log(age);
+	
+});
 app.listen(3001)
